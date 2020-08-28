8b238eed65762f572b3cc2514947ac7d048005b4
littleDrive <ZERO.OU@OOCL.COM>
2020-08-28 18:19:58 +0800

    [homework][zero]feature: add test voyageRisk case 4. should return 4 when voyageRisk given voyage length = 9 and voyage zone=china

--- test/rankTest.js
+++ test/rankTest.js
@@ -45,3 +45,12 @@ rankTest('voyageRisk case 3. should return 4 when voyageRisk given voyage length
     const result = voyageRisk(voyage);
     t.is(result, 4);
 });
+
+rankTest('voyageRisk case 4. should return 4 when voyageRisk given voyage length = 9 and voyage zone=china', t => {
+    const voyage = {
+        zone: 'china',
+        length: 9,
+    };
+    const result = voyageRisk(voyage);
+    t.is(result, 8);
+});
