6d26a7f42909fe0e1c6476b8f3f2d46d24fdbb29
littleDrive <2682155146@qq.com>
2020-08-29 00:11:58 +0800

    [homework][zero]refactor: remove unneccesary code

--- src/rank.js
+++ src/rank.js
@@ -74,30 +74,6 @@ function rating (voyage, history) {
   }
 }
 
-const voyage = {
-  zone: 'west-indies',
-  length: 10,
-};
-const history = [
-  {
-    zone: 'east-indies',
-    profit: 5,
-  },{
-    zone: 'west-indies',
-    profit: 15,
-  },{
-    zone: 'china',
-    profit: -2,
-  },
-  {
-    zone: 'west-africa',
-    profit: 7,
-  },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
-
-
 module.exports = {
     voyageRisk,
     hasChina,
